407ac079160f7ea3e46369d961ac05d9eaf497e8
Sarthak Mhase <mhasesarthak62@gmail.com>
2024-04-07 10:01:46 +0530

    first commit

---
+++ Streamlit_Frontend/Hello.py
@@ -0,0 +1,55 @@
+# import streamlit as st
+
+# st.set_page_config(
+#     page_title="Hello",
+#     page_icon="👋",
+# )
+
+# st.write(" Hello, we are team 404 NOT FOUND ! 👋")
+
+
+# st.sidebar.success("CODEMENT : national level hackathon")
+# st.sidebar.success("Welcome to Food Recommendation System.")
+# st.write("Theme : FOOD & HEALTHCARE")
+# st.write("Problem Statement :")
+# st.markdown("Develop a solution that helps individuals track their daily nutritional intake and provides personalized recommendations for a healthier lifestyle.")
+# st.write("Solution : ")
+# st.markdown(
+#     """
+#     A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
+#     You can find more details and the whole project on our [repo](https://github.com/samarthhapse/HM0034_404NOTFOUND).
+#     """
+# )
+
+# st.write("Team Members 👋")
+# st.write("Samarth Hapse")
+# st.write("Sarthak Mhase")
+# st.write("Atharv Khunte")
+import requests
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.sidebar.success("CODEMENT : National level hackathon")
+st.sidebar.success("Welcome to Food Recommendation System.")
+st.write("# CODEMENT 2K24")
+st.write(" ## National level hackathon")
+st.write("Theme : FOOD & HEALTHCARE")
+
+st.write("Problem Statement :")
+st.markdown("Develop a solution that helps individuals track their daily nutritional intake and provides personalized recommendations for a healthier lifestyle.")
+st.write("Solution : ")
+st.markdown(
+    """
+    A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
+    You can find more details and the whole project on our [repo](https://github.com/Sarthakmhase/codement_2024).
+    """
+)
+st.write("--------------------------------------------------------------------------------------------------------")
+st.write(" Hello, we are team 404 NOT FOUND ! 👋")
+st.write("Samarth Hapse")
+st.write("Sarthak Mhase")
+st.write("Atharv Khunte")
